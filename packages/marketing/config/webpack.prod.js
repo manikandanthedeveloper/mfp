66a4e18d48334a71c7ce664da3f3ef38972a11ad
Manikandan Dhanapal <manikandanthedeveloper@gmail.com>
2025-12-03 10:50:58 +0530

    Update marketing workflow, webpack config and index.html

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -10,6 +10,7 @@ const prodConfig = {
 	mode: "production",
 	output: {
 		filename: "[name].[contenthash].js",
+		publicPath: `${domain}/marketing/latest/`,
 	},
 	plugins: [
 		new webpack.DefinePlugin({
